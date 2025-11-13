37a4511f9600484c3cb328054c162d9e9408a9b7
jprodrigues644 <jprodrigues644@gmail.colm>
2025-11-13 08:31:45 +0100

    style

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,27 +21,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
 
-  //   // Lack of strict typing 
-  //  // this.http.get<any[]>(this.olympicUrl).pipe().subscribe(
-  //     (data) => {
 
-  //       // Console log , must be deleted later
-  //       console.log(`Liste des données : ${JSON.stringify(data)}`);
-  //       if (data && data.length > 0) {
-  //         this.totalJOs = Array.from(new Set(data.map((i: any) => i.participations.map((f: any) => f.year)).flat())).length;
-  //         const countries: string[] = data.map((i: any) => i.country);
-  //         this.totalCountries = countries.length;
-  //         const medals = data.map((i: any) => i.participations.map((i: any) => (i.medalsCount)));
-  //         const sumOfAllMedalsYears = medals.map((i) => i.reduce((acc: any, i: any) => acc + i, 0));
-  //         this.buildPieChart(countries, sumOfAllMedalsYears);
-  //       }
-  //     },
-  //     (error:HttpErrorResponse) => {
-  //       console.log(`erreur : ${error}`);
-  //       this.error = error.message
-  //     }
-  //   )
-  // }
   this.olympics$ = this.dataService.getDataOlympics();
   this.totalJOs$ = this.dataService.getTotalJOs();
   this.medalsByCountry$ = this.dataService.getMedalsByCountry();
